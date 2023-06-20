a714ee6934a24252a162786a937582039daa03bd
yjp1406 <yagneshpatelyjp1406@gmail.com>
2023-06-20 11:57:45 +0530

    blogify added

---
+++ routes/blog.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const path = require("path");
+
+const Blog = require("../models/blog")
+const Comment = require("../models/comment")
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, `./public/uploads/`)
+    },
+    filename: function (req, file, cb) {
+      const fileName = `${Date.now()} -${file.originalname}`
+      cb(null,fileName)
+    }
+  });
+
+  const upload = multer({ storage: storage })
+  
+
+
+router.get("/add-new", (req,res) => {
+    return res.render("addBlog", {
+        user: req.user
+    });
+})
+
+router.get("/:id",async (req,res) => {
+    const blog = await Blog.findById(req.params.id).populate("createdBy");
+    const comments = await Comment.find({blogId: req.params.id}).populate("createdBy");
+    // console.log(blog);
+    console.log("comments", comments)
+    return res.render("blog",{
+        user: req.user,
+        blog,
+        comments
+    });
+})
+
+router.post("/comment/:blogId", async (req,res) => {
+    console.log(req.params.blogId)
+    await Comment.create({
+        content: req.body.content,
+        blogId: req.params.blogId,
+        createdBy: req.user._id
+    });
+
+    return res.redirect(`/blog/${req.params.blogId}`)
+})
+
+router.post("/", upload.single('coverImage'), async (req,res) => {
+    const {title, body} = req.body;
+    const blog = await Blog.create({
+        body,
+        title,
+        createdBy: req.user._id,
+        coverImageURL: `/uploads/${req.file.filename}`
+    })
+    return res.redirect(`/blog/${blog._id}`);
+})
+
+
+module.exports = router;
---
+++ routes/user.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const User = require('../models/user')
+const router = express.Router();
+
+
+router.get('/signin', (req,res) => {
+    return res.render("signin");
+});
+
+router.get('/signup', (req,res) => {
+    return res.render("signup");
+});
+
+router.post('/signin', async (req,res) => {
+    const {email,password} = req.body;
+    try {
+        const token = await User.matchPasswordAndGenerateToken(email,password);   
+        console.log('token', token); 
+        return res.cookie("token",token).redirect("/");
+    } catch (error) {
+        return res.render("signin", {error: "Incorrect email or password"});
+    }
+})
+
+router.post('/signup', async (req,res) => {
+    const {fullName, email, password} = req.body;
+    await User.create({
+        fullName,
+        email,
+        password,
+    });
+    return res.redirect("/");
+});
+
+router.get("/logout", (req,res) => {
+    res.clearCookie("token").redirect("/")
+})
+
+
+module.exports = router;
